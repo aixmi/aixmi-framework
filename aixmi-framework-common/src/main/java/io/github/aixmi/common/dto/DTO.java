21ce6fd3e0dbdee77fb72e4fef0f306d25bfa302
machuqiji <machuqiji@163.com>
2020-11-17 17:47:38 +0800

    add: first release on the oss done!

---
+++ aixmi-framework-common/src/main/java/io/github/aixmi/common/dto/DTO.java
@@ -0,0 +1,8 @@
+package io.github.aixmi.common.dto;
+
+/**
+ * @author mcqj
+ * @date 2020-11-17 下午 12:55
+ */
+public class DTO {
+}
---
+++ aixmi-framework-tool/src/main/java/io/github/aixmi/tool/StringUtil.java
@@ -0,0 +1,11 @@
+package io.github.aixmi.tool;
+
+/**
+ * @author mcqj
+ * @date 2020-11-17 下午 12:56
+ */
+public class StringUtil {
+	public static boolean isEmpty(String source) {
+		return source == null || source.isEmpty();
+	}
+}
