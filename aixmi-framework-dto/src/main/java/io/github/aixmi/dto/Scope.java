95f229d2b26a6b42ecca588f44626eb6c9c934cb
machuqiji <machuqiji@163.com>
2021-01-04 13:38:57 +0800

    状态机

---
+++ aixmi-framework-dto/src/main/java/io/github/aixmi/dto/Scope.java
@@ -0,0 +1,10 @@
+package io.github.aixmi.dto;
+
+/**
+ * 结果范围控制
+ *
+ * @author xiaochu.lbj
+ */
+public abstract class Scope extends DTO {
+
+}
---
+++ aixmi-framework-statemachine/src/main/java/io/github/aixmi/statemachine/builder/On.java
@@ -0,0 +1,20 @@
+package io.github.aixmi.statemachine.builder;
+
+
+import io.github.aixmi.statemachine.Condition;
+
+/**
+ * On
+ *
+ * @author Frank Zhang
+ * @date 2020-02-07 6:14 PM
+ */
+public interface On<S, E, C> extends When<S, E, C> {
+	/**
+	 * Add condition for the transition
+	 *
+	 * @param condition transition condition
+	 * @return When clause builder
+	 */
+	When<S, E, C> when(Condition<C> condition);
+}
